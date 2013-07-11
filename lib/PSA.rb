3f1797757dd8f487dd3d4528a35c83fab022af5d
Oren Mazor <oren.mazor@gmail.com>
2013-07-11 15:31:27 -0400

    lol

---
+++ lib/PSA.rb
@@ -0,0 +1,24 @@
+require "PSA/version"
+require "net/http"
+
+module Intercom
+  class PSA
+    def initialize(uri)
+      @uri = uri
+    end
+
+    def broadcast(msg,voice=nil)
+      url = URI('http://intercom.shopifyapps.com')
+      msg = "#{msg} -v #{voice}" if voice
+      url.query = URI.encode_www_form({:message=>"#{msg}"})
+
+      res = Net::HTTP.get_response(url)
+      puts res.body if res.is_a?(Net::HTTPSuccess)
+    end
+
+    def alert(length=1)
+      msg = "ah " * length
+      broadcast(msg,"Bells")
+    end
+  end
+end
---
+++ lib/PSA/version.rb
@@ -0,0 +1,3 @@
+module PSA
+  VERSION = "0.0.1"
+end
